00ab26cd50da7bd69910dd17fee7bfae8a7cdc92
hanukoon <hwymaster01@gmail.com>
2019-03-29 09:16:40 +0900

    aaa

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 class li(db.Model):
-    __table_name__ = 'li'
+    __table_name__ = 'table'
     db_id = db.Column(db.Integer, primary_key=True)
     db_input = db.Column(db.String(80), unique=True, nullable=False)
     db_output = db.Column(db.String(120), unique=True, nullable=False)
